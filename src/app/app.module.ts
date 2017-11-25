7ddf210c713c241a6e9bf6422a45e956d196d831
fernandom06 <fernandom06@hotmail.com>
2017-11-25 17:19:45 +0100

    Enlace con votos definidos en el constructor del componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticuloComponent } from './articulo/articulo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ArticuloComponent,
   ],
   imports: [
     BrowserModule
